ba653352d4693a49ad8e1f54537c81bbf2f7db2e
rivanrivaldi21 <rivanaldi86@gmail.com>
2022-10-17 20:50:24 +0700

    First Commit

---
+++ addons/approval_custom/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Approvals',
+    'version': '0.1',
+    'summary': 'Create and validate approval requests
+    '
+}
